beedfc352d98f56fe8ea4dc52e39b52236b69d47
Ian Livingstone <ianrlivingstone@gmail.com>
2025-08-03 13:13:41 -0300

    Switch to absolute line numbers

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Neovim options and settings
 -- Basic settings
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
